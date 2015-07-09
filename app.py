7b81d1ff8e416e0ce085ee3df4844d609df78b5b
Omer <omerouz@gmail.com>
2015-07-09 18:15:30 +0300

    admin page bugs fixed

--- app.py
+++ app.py
@@ -1,53 +1,73 @@
 from bson import ObjectId
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for
 from pymongo import MongoClient
+from flask import request, abort
+from datetime import datetime
+
 
 client = MongoClient('mongodb://localhost:27017/')
-db = client.( db name )
+db = client.chroma
 app = Flask(__name__)
 page = 0
 page_end = 15
 
-
-@app.route('/next')
-def next_page():
-    global page, page_end
-    page += 15
-    page_end += 15
-    results = list(db.logs.find())
-    last = results[page:page_end]
-    objects = []
-
-
-    for i in last:
-        item = {}
-        item['time'] = i.get('timestamp')
-        item['username'] = i.get('username')
-        item['url'] = i.get('url')
-        item['id'] = i.get('_id')
-        objects.append(item)
-    return render_template('index.html', objects=objects, last=last)
-
-
 @app.route('/')
 def home_page():
-    results = list(db.logs.find())
-    last = results[page:page_end]
+    offset = request.args.get('offset')
+
+    if offset:
+        results = list(db.logs.find({'_id': {'$lt': ObjectId(offset)}}).sort('_id', -1).limit(15))
+    else:
+        results = list(db.logs.find().sort('_id', -1).limit(15))
+
+    offset_id = results[14].get('_id')
+
+    #last = results[page:page_end]
     objects = []
 
-    for i in last:
+
+    for i in results:
         item = {}
         item['time'] = i.get('timestamp')
         item['username'] = i.get('username')
         item['url'] = i.get('url')
         item['id'] = i.get('_id')
         objects.append(item)
-    return render_template('index.html', objects=objects, last=last)
+    return render_template('index.html', objects=objects, results=results, offset=offset_id)
 
+
+def time_format(datetime_):
+    dt = datetime.strptime(datetime_, "%Y-%m-%dT%H:%M")
+    return dt
+
+@app.route('/search/')
+def search():
+    date_from = request.args.get('date_from')
+    date_to = request.args.get('date_to')
+    response = db.logs.find({'$and': [{'timestamp': {'$gt': time_format(date_from)}},
+                                      {'timestamp': {'$lt': time_format(date_to)}}]}).sort('_id', -1)
+    if response:
+        objects = []
+        results = list(response)
+        for i in results:
+            item = {}
+            item['time'] = i.get('timestamp')
+            item['username'] = i.get('username')
+            item['url'] = i.get('url')
+            item['id'] = i.get('_id')
+            objects.append(item)
+
+        return render_template('index.html', objects=objects,
+                                             results=results,
+                                             has_next= len(objects))
+
+
+    else:
+        abort(404)
 
 @app.route('/detail/<id>')
 def detail(id):
-    return render_template('detail.html', id=id)
+    return render_template('detail.html', id=id, redirect=redirect_url())
 
 @app.route('/body/<id>')
 def get_detail(id):
@@ -65,6 +85,10 @@ def get_detail(id):
                    remoteip = data['remote_ip']
                    )
 
+def redirect_url(default='index'):
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for(default)
 
 if __name__ == '__main__':
     app.debug = True
